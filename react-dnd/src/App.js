9d509e89838cea4509444474d6f78b4ce0679b9f
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-04-27 10:18:08 +0200

    Create example boards and cards

--- react-dnd/src/App.js
+++ react-dnd/src/App.js
@@ -1,7 +1,20 @@
+import Board from './components/Board';
+import Card from './components/Card';
+
 function App() {
   return (
     <div className="App">
       <main className="flexbox">
+        <Board id="board-1" className="board">
+          <Card id="card-1" classname="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+        </Board>
+        <Board id="board-2" className="board">
+          <Card id="card-2" classname="card" draggable="true">
+            <p>Card two</p>
+          </Card>
+        </Board>
       </main>
     </div>
   );
