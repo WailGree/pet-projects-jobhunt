fa3a14ed97a3c91aa09fd10848f5f9cf365da209
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-04-27 11:38:51 +0200

    Fix typo

--- react-dnd/src/App.js
+++ react-dnd/src/App.js
@@ -6,12 +6,12 @@ function App() {
     <div className="App">
       <main className="flexbox">
         <Board id="board-1" className="board">
-          <Card id="card-1" classname="card" draggable="true">
+          <Card id="card-1" className="card" draggable="true">
             <p>Card one</p>
           </Card>
         </Board>
         <Board id="board-2" className="board">
-          <Card id="card-2" classname="card" draggable="true">
+          <Card id="card-2" className="card" draggable="true">
             <p>Card two</p>
           </Card>
         </Board>
