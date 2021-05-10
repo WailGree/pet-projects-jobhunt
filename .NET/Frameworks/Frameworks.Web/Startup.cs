000029b48b2160d4d88634ae8d8afde6db5d43dd
WailGree <szilagyi.roland.dominik@gmail.com>
2021-05-10 18:41:10 +0200

    Remove template code & elements

--- .NET/Frameworks/Frameworks.Web/Startup.cs
+++ .NET/Frameworks/Frameworks.Web/Startup.cs
@@ -1,16 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Frameworks.Web
 {
@@ -28,10 +20,6 @@ namespace Frameworks.Web
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Frameworks.Web", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +28,6 @@ namespace Frameworks.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Frameworks.Web v1"));
             }
 
             app.UseHttpsRedirection();
