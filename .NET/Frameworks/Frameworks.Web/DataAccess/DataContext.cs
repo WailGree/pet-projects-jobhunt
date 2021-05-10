4cdd27617208339ae61f3cd32c6bef66622750e7
WailGree <szilagyi.roland.dominik@gmail.com>
2021-05-10 22:37:20 +0200

    Create Element class and DataContext

---
+++ .NET/Frameworks/Frameworks.Web/DataAccess/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Frameworks.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RemailCore.Library.DataAccess
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+        public DbSet<Element> Elements { get; set; }
+    }
+}
---
+++ .NET/Frameworks/Frameworks.Web/Models/Element.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Frameworks.Web.Models
+{
+    public class Element
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(256)]
+        public string Name { get; set; }
+        [MaxLength(1024)]
+        public string Description { get; set; }
+    }
+}
