2baabf9b0a41c47e9d59f17bf72a9caddb0ca096
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-11 14:24:42 +0200

    Made properties required

--- .NET/Frameworks/EFDataAccessLibrary/Models/Element.cs
+++ .NET/Frameworks/EFDataAccessLibrary/Models/Element.cs
@@ -4,11 +4,8 @@ namespace Frameworks.Web.Models
 {
     public class Element
     {
-        [Key]
-        public int Id { get; set; }
-        [MaxLength(256)]
-        public string Name { get; set; }
-        [MaxLength(1024)]
-        public string Description { get; set; }
+        [Key] [Required] public int Id { get; set; }
+        [MaxLength(256)] [Required] public string Name { get; set; }
+        [MaxLength(1024)] [Required] public string Description { get; set; }
     }
 }
