970831b92e0c781c77ce6cc14488fb9fd80e7ea3
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-11 15:13:23 +0200

    Add migration script for optimized columns in Element

--- .NET/Frameworks/EFDataAccessLibrary/Migrations/20210511130447_ElementTableOptimization.Designer.cs
+++ .NET/Frameworks/EFDataAccessLibrary/Migrations/20210511130447_ElementTableOptimization.Designer.cs
@@ -3,14 +3,16 @@ using EFDataAccessLibrary.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EFDataAccessLibrary.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210511130447_ElementTableOptimization")]
+    partial class ElementTableOptimization
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
