7eed156461fae46a5dd30e74da2d29c995be7e05
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-12 10:43:22 +0200

    DataContext DI

--- .NET/Frameworks/Frameworks.Web/Controllers/ElementsController.cs
+++ .NET/Frameworks/Frameworks.Web/Controllers/ElementsController.cs
@@ -3,14 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFDataAccessLibrary.DataAccess;
 
 namespace Frameworks.Web.Controllers
 {
+    [ApiController]
+    [Route("elements")]
     public class ElementsController : Controller
     {
+        private readonly DataContext _db;
+
+        public ElementsController(DataContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return Ok();
         }
     }
 }
