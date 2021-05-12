4008c691933a38d4340da785bef434fe1d2af5b8
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-12 10:49:59 +0200

    Load sample data if db empty

--- .NET/Frameworks/Frameworks.Web/Controllers/ElementsController.cs
+++ .NET/Frameworks/Frameworks.Web/Controllers/ElementsController.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
 
 namespace Frameworks.Web.Controllers
 {
@@ -16,6 +18,16 @@ namespace Frameworks.Web.Controllers
         public ElementsController(DataContext db)
         {
             _db = db;
+            LoadSampleData();
+        }
+
+        private void LoadSampleData()
+        {
+            if (_db.Elements.Any()) return;
+            var file = System.IO.File.ReadAllText("generated.json");
+            var elements = JsonSerializer.Deserialize<List<Element>>(file);
+            _db.AddRange(elements);
+            _db.SaveChanges();
         }
 
         public IActionResult Index()
