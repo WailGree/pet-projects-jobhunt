74dd85eefc9a08dc73cbdace39fa0becd9baf108
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-14 15:20:35 +0200

    Make use of 'Navbar' component

--- React/frameworks-frontend/src/components/App.jsx
+++ React/frameworks-frontend/src/components/App.jsx
@@ -1,7 +1,12 @@
+import '../style/App.css';
+import Navbar from './Navbar';
+import { Redirect, Route } from "react-router-dom";
+
 export default function App() {
   return (
     <div className="App">
-
+      <Redirect exact from="/" to="/home" />
+      <Route exact path="/:page?" render={props => <Navbar {...props} />} />
     </div>
   )
 }
