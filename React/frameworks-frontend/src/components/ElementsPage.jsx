394cddc722719f2aa9a711d27cc440b7d225347c
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-05-14 15:51:51 +0200

    Create responsive grid for displaying elements

--- React/frameworks-frontend/src/components/ElementsPage.jsx
+++ React/frameworks-frontend/src/components/ElementsPage.jsx
@@ -1,8 +1,41 @@
+import Element from "./Element";
+import { Grid, makeStyles } from '@material-ui/core';
+
 export default function ElementsPage() {
-    
+
+    const gridItemxl = 2;
+    const gridItemlg = 3;
+    const gridItemmd = 4;
+    const gridItemsm = 5;
+    const gridItemxs = 12;
+
+    const useStyle = makeStyles({
+        root: {
+            flexGrow: 1,
+            padding: '5%' //Fixes page overflow
+        },
+        containerGrid: {
+            paddingLeft: '20px',
+            paddingRight: '20px',
+            justifyContent: 'flex-start',
+            alignItems: 'flex-start'
+        }
+    })
+    const classes = useStyle();
+
+    let exampleElements = [];
+    for (let i = 1; i <= 100; i++) {
+        exampleElements.push({ name: "Test" + i, description: "This is test " + i })
+    }
+    let example = exampleElements.map((exampleElement, index) => (
+        <Grid item key={index} xs={gridItemxs} xl={gridItemxl} lg={gridItemlg} sm={gridItemsm} md={gridItemmd}>
+            <Element element={exampleElement} />
+        </Grid>
+    ))
+
     return (
-        <div>
-            This is the Elements page
-        </div>
+        <Grid container spacing={6} className={classes.containerGrid}>
+            {example}
+        </Grid>
     )
 }
